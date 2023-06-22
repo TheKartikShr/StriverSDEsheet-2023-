61752e670fd52ab64875b617ea893a7676ca36d9
Kartik Sharma <103728816+TheKartikShr@users.noreply.github.com>
2023-06-22 19:20:05 +0530

    Update Check_Bipartite_Graph.cpp

--- Check_Bipartite_Graph.cpp
+++ Check_Bipartite_Graph.cpp
@@ -4,7 +4,7 @@ using namespace std;
 bool dfs(int node, vector<int> adj[], vector<int> &color){
 	for(auto it : adj[node]){
 		if(color[it] == -1){
-			color[it] = 1 - color[node];
+			color[it] = !color[node];
 			if(!dfs(it, adj, color)){
 				return false;
 			}
