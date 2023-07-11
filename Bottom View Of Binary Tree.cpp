74db5808f9d38d95c73d62dcb4655a48b7f49625
Kartik Sharma <103728816+TheKartikShr@users.noreply.github.com>
2023-07-11 15:50:12 +0530

    Add files via upload

---
+++ Bottom View Of Binary Tree.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+struct Node{
+	int data;
+	Node* left, *right;
+	Node(int data){
+		this -> data = data;
+		this -> left = NULL;
+		this -> right = NULL;
+	}
+};
+
+vector<int> bottomView(Node * root){
+
+    vector<int> ansvec;
+    if(root == NULL){
+        return ansvec;
+    }
+    map<int, int> mp1;
+    queue<pair<Node *, int>> q;
+    q.push({root, 0});
+    while(!q.empty()){
+        pair<Node *, int> pq = q.front();
+        q.pop();
+        Node * temp = pq.first;
+        int lvl = pq.second;
+        mp1[lvl] = temp -> data;
+        if(temp -> left){
+            q.push({temp -> left, lvl - 1});
+        }
+        if(temp -> right){
+            q.push({temp -> right, lvl + 1});
+        }
+    }
+    for(auto it : mp1){
+        ansvec.push_back(it.second);
+    }
+    return ansvec;
+    
+}
+
+
+
+int main(){
+	Node* root = new Node(1);
+	root -> left = new Node(2);
+	root -> right = new Node(3);
+	root -> left -> left = new Node(4);
+	root -> left -> right = new Node(5);
+	root -> right -> left = new Node(6);
+	root -> right -> right = new Node(7);
+	root -> left -> left -> left = new Node(8);
+	root -> left -> left -> right = new Node(9);
+	vector<int> curr = bottomView(root);
+	for(auto it : curr){
+		cout << it << " ";
+	}
+	cout << endl;
+}
